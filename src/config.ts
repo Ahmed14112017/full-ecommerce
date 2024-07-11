d9b12861be14e6e728fdf9bd42a92b7951050b0e
ahmed arafa <70607269+Ahmed14112017@users.noreply.github.com>
2024-07-11 16:07:25 +0300

    a

---
+++ src/config.ts
@@ -0,0 +1,25 @@
+
+interface config{
+baseurl:string
+}
+const checkConfig=(server:string):config | {}=>{
+    let config:config | {}={}
+    switch (server){
+        case "production":
+            config={
+                baseurl:"",
+
+            }
+            break;
+            case "local":
+                config={
+                    baseurl:"http://localhost:8000/"
+                };
+                break;
+                default:
+                break;
+    }
+    return config 
+}
+export const selectserver="local";
+export const config= checkConfig(selectserver)as config;
